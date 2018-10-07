a33e0cd56edd1e69538491414f267b21fe1360a7
rhindere <rhindere@cisco.com>
2018-10-07 09:23:03 -0700

    Update python_examples/maple_get_put.py

--- python_examples/maple_get_put.py
+++ python_examples/maple_get_put.py
@@ -34,7 +34,7 @@ from pprint import pprint
 maple_tree = MapleTree(logging_level='INFO', name='amp_test', tree_dir=r'C:\Users\rhindere\Documents\maple_working_dir')
 model_json_file=r'C:\Users\rhindere\Documents\PycharmProjects\maple_project\maple\fmc\api-docs-fmcwithll.json'
 FMC_leaf = maple_tree.add_leaf_instance('fmc',name='10.1.101.39_Global', json_file_path=model_json_file,
-                                        FMC_host='10.1.101.39', FMC_username='rest_admin',FMC_password='C1sc0123',
+                                        FMC_host='10.1.101.39', FMC_username='rest_admin', FMC_password='C1sc0123',
                                         default_get_item_limit=200)
 
 # Use smart_get_object_id to retrieve object ids by objectpath query...
